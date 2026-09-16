6c4d559b0a8dab796fbd3d70cfab796090b70b72
Abdul Kadir <abdulkadir14781@gmail.com>
2026-09-16 09:30:57 +0530

    Update perfect_square.py

--- FUNCTION PROBLEM/perfect_square.py
+++ FUNCTION PROBLEM/perfect_square.py
@@ -1,1 +1,9 @@
-initial commit
+def perfect_square(n):
+    root=n**(0.5) 
+    return root
+n=int(input("num: "))    
+result =perfect_square(n)
+if result == int(result):
+    print("perfect_square")
+else:
+    print("Not Perfect Square")    
